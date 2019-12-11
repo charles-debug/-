f7a9054609e926244a62e4a010802a2a11fd6217
charles-debug <57556785+charles-debug@users.noreply.github.com>
2019-12-11 09:50:09 +0800

    Add files via upload

---
+++ xiaoshuo.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+import lxml
+
+url = "http://www.0376h.com/cbook_2056/1.html"
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36"
+}
+res = requests.get(url = url, headers = headers)
+
+html = res.text
+soup = BeautifulSoup(html, "lxml")
+r = soup.find("div", id = "content")
+texts = r.text.replace('\xa0'*4, '\n\n')
+print(texts)
+with open('xiaoshuo.txt', 'w') as f:
+    f.write(texts)
+f.close()
